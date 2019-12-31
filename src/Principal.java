c8b834af0c4e33115712bcc30c58e65bedf24f76
Thiago Rocha <thiagorocha503@gmail.com>
2019-12-31 16:39:20 -0300

    :tada:Initial commit

---
+++ src/Principal.java
@@ -0,0 +1,20 @@
+
+import view.JanelaListagem;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author thiago
+ */
+public class Principal {
+    
+    public static void main(String[] arg){
+        JanelaListagem janelaListagem = new JanelaListagem();
+        janelaListagem.setVisible(true);
+    }
+}
---
+++ src/model/Emprestimo.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author thiago
+ */
+public class Emprestimo {
+    private String item;
+    private String dataEmprestimo;
+    private String dataDevolucao;
+    private String dataDevolvido;
+    private String AmigoContato;
+    private String AmigoNome;
+
+    public Emprestimo() {
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(String dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public String getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(String dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public String getDataDevolvido() {
+        return dataDevolvido;
+    }
+
+    public void setDataDevolvido(String dataDevolvido) {
+        this.dataDevolvido = dataDevolvido;
+    }
+
+    public String getAmigoContato() {
+        return AmigoContato;
+    }
+
+    public void setAmigoContato(String AmigoContato) {
+        this.AmigoContato = AmigoContato;
+    }
+
+    public String getAmigoNome() {
+        return AmigoNome;
+    }
+
+    public void setAmigoNome(String AmigoNome) {
+        this.AmigoNome = AmigoNome;
+    }
+
+    @Override
+    public String toString() {
+        return "Emprestimo{" + "item=" + item + ", dataEmprestimo=" + dataEmprestimo + ", dataDevolucao=" + dataDevolucao + ", dataDevolvido=" + dataDevolvido + ", AmigoContato=" + AmigoContato + ", AmigoNome=" + AmigoNome + '}';
+    }
+    
+    
+    
+    
+}
