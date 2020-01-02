9e9158c2fd14e36aa3ab33beb0de30c068a0cf0d
Thiago Rocha <thiagorocha503@gmail.com>
2020-01-02 11:30:27 -0300

    :sparkles:Adiciona métodos para fechar conexões

--- src/connection/ConnectionFatory.java
+++ src/connection/ConnectionFatory.java
@@ -5,7 +5,12 @@
  */
 package connection;
 
+import java.io.File;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.JOniException;
 
 /**
  *
@@ -16,21 +21,52 @@ public class ConnectionFatory {
     private static final String PATH ="jdbc:sqlite:" + System.getProperty("user.dir")+"\\src\\data\\dataBase.db";
     
     public static Connection  getConnection(){
-       
         try {
             Class.forName("org.sqlite.JDBC");
             Connection conn = DriverManager.getConnection(ConnectionFatory.PATH);
             //System.out.println("Coneção realizada com sucesso!");
             return conn;
         } catch (SQLException ex) {
-            System.err.println("Erro: "+ex);
+            System.err.println("Erro SQLException "+ex);
             return null;
         } catch (ClassNotFoundException ex) {
-            System.err.println("Erro: "+ex);
+            System.err.println("Erro ClassNotFound: "+ex);
             return null;
         }
     }
     
+    
+    public static void closeConnection(Connection conn){
+        if(conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                throw new RuntimeException("Erro: "+ex);
+            }
+        }
+    }
+    
+    public static void closeConnection(Connection conn, PreparedStatement stmt) {
+        ConnectionFatory.closeConnection(conn);
+        if (stmt != null){
+            try {
+                stmt.close();
+            } catch (SQLException ex) {
+                throw new RuntimeException("Erro: "+ex);
+            }
+        }
+    }
+    
+    public static void closeConnection(Connection conn, PreparedStatement stmt, ResultSet st){
+       ConnectionFatory.closeConnection(conn, stmt);
+        if (stmt != null){
+            try {
+                st.close();
+            } catch (SQLException ex) {
+                throw new RuntimeException("Erro: "+ex);
+            }
+        }
+    }
     public static void main(String[] args){
         Object r = ConnectionFatory.getConnection();
     }
