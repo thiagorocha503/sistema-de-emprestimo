a6deed689b1b84b09ed3e62e6f500bc0ea945948
Thiago Rocha <thiagorocha503@gmail.com>
2020-01-05 14:57:21 -0300

    :recycle:Refatora tratamento de exceções

--- src/view/DialogDevolucao.java
+++ src/view/DialogDevolucao.java
@@ -153,15 +153,11 @@ public class DialogDevolucao extends javax.swing.JDialog {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(DialogDevolucao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(DialogDevolucao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(DialogDevolucao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(DialogDevolucao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        
         //</editor-fold>
 
         /* Create and display the dialog */
--- src/view/DialogEmprestimoCadastro.java
+++ src/view/DialogEmprestimoCadastro.java
@@ -21,7 +21,7 @@ public class DialogEmprestimoCadastro extends javax.swing.JDialog {
      * Creates new form DialogEmprestimo
      */
 
-    private EmprestimoController controller;
+    private final EmprestimoController controller;
     
     public DialogEmprestimoCadastro(java.awt.Frame parent, boolean modal, EmprestimoController controller) {
         super(parent, modal);       
@@ -283,15 +283,12 @@ public class DialogEmprestimoCadastro extends javax.swing.JDialog {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(DialogEmprestimoCadastro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(DialogEmprestimoCadastro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(DialogEmprestimoCadastro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(DialogEmprestimoCadastro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        
         //</editor-fold>
         //</editor-fold>
 
